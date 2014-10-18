d02437a509265d098baa5285fdb68aed7874bef0
Delbert <Delbert@MBA.local>
2014-10-19 00:42:55 +0800

    SimpleValues Playground

---
+++ SimpleValues.playground/section-1.swift
@@ -0,0 +1,29 @@
+// Playground - noun: a place where people can play
+
+//import Cocoa
+
+//var str = "Hello, playground"
+
+var myVariable = 42
+myVariable = 50
+let myCOnstant = 42
+
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let expFloat : Float = 4
+
+// Values are never implicitly converted to another type
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width) // Cannot invoke + ... error
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+
+
