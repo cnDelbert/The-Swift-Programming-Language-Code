2e99303b083db1d069704a17896639bdb4ccfd3e
Delbert <code@delbert.me>
2014-10-19 22:59:53 +0800

    Finish Control Flow

--- ControlFlow.playground/section-1.swift
+++ ControlFlow.playground/section-1.swift
@@ -25,8 +25,75 @@ optionalString == nil // not nil
 
 var optionalName: String? = "John Appleseed"
 var greeting = "Hello"
-if let name = optionalName {
+if let name = optionalName {    // optional vlaue nil makes conditional false
     greeting = "Hello, \(name)"
 } else {
     println("optionalName is nil.")
 }
+
+
+// Switch-case
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    let vegetableComment = "Add some raisins and make ants on a log."
+case "cucumber", "watercress":
+    let vegetableComment = "That would make a good tea sanwich."
+case let x where x.hasSuffix("pepper"):
+    let vegetableComment = "Is it a spicy \(x)"
+default:
+    let vegetableComment = "Everything tastes good in soup."
+}   // A default case is needed.
+
+
+// For-in
+let interestingNumbers = [
+    "Prime" : [2, 3, 5, 7, 11, 13],
+    "Fibonacci" : [1, 1, 2, 3, 5, 8],
+    "Square" :  [1, 4, 9, 16, 25],
+]
+
+var largest = 0
+var lgkind = "" // for the largest key
+for (kind, numbers) in interestingNumbers {
+    for k in interestingNumbers.keys {
+        lgkind = k
+        for number in numbers {
+            if number > largest {
+                largest = number
+            }
+        }
+    }
+}
+
+lgkind
+largest
+
+
+// While and do-while
+var n = 2
+while n < 100 {
+    n *= 2  // Alternative for n = n * 2
+}
+n
+
+var m = 2
+do {
+    m *= 2  // Auto-indent may have some problems
+} while m < 100
+m
+
+// For loop
+var firstForLoop = 0
+for i in 0..<4 {
+    firstForLoop += i
+}
+firstForLoop
+
+var secondForLoop = 0
+for var i = 0; i < 4; ++i {
+    secondForLoop += i
+}
+secondForLoop
+
+// ..< without upper value while ... contains both values.
